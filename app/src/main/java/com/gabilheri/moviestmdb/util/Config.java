8285443c6f21d7d47dbb9f9f966a38d25cdd3250
Phong Huỳnh <p7157568@gmail.com>
2017-09-14 15:15:44 +0700

    feat - add api key for movie db

---
+++ app/src/main/java/com/gabilheri/moviestmdb/util/Config.java
@@ -0,0 +1,10 @@
+package com.gabilheri.moviestmdb.util;
+
+/**
+ * Created by CPU11112-local on 9/14/2017.
+ */
+
+public class Config {
+    public static final String API_KEY_URL = "53470b56a60668274e1dd9f84d882564";
+
+}
