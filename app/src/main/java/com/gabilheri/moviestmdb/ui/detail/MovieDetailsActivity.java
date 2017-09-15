c8efad79320e0c26f4b7e6ada2d962c094fb8bc9
Phong Huỳnh <p7157568@gmail.com>
2017-09-15 14:12:44 +0700

    feat - add template files, add movie fragment in activiyt

---
+++ app/src/main/java/com/gabilheri/moviestmdb/ui/detail/MovieDetailsActivity.java
@@ -0,0 +1,36 @@
+package com.gabilheri.moviestmdb.ui.detail;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+
+import com.gabilheri.moviestmdb.R;
+import com.gabilheri.moviestmdb.dagger.modules.HttpClientModule;
+import com.gabilheri.moviestmdb.data.models.Movie;
+import com.gabilheri.moviestmdb.ui.base.BaseTvActivity;
+import com.gabilheri.moviestmdb.ui.base.GlideBackgroundManager;
+
+/**
+ * Created by CPU11112-local on 9/15/2017.
+ */
+
+public class MovieDetailsActivity extends BaseTvActivity {
+    GlideBackgroundManager mBackgroundManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Retrieve the movie through the intent
+        Movie movie = getIntent().getExtras().getParcelable(Movie.class.getSimpleName());
+        MovieDetailsFragment detailsFragment = MovieDetailsFragment.newInstance(movie);
+        addFragment(detailsFragment); // Method from BaseTvActivity
+
+        // Sets the background of the activity to the backdrop of the movie
+        mBackgroundManager = new GlideBackgroundManager(this);
+        if (movie != null && movie.getBackdropPath() != null) {
+            mBackgroundManager.loadImage(HttpClientModule.BACKDROP_URL + movie.getBackdropPath());
+        } else {
+            mBackgroundManager.setBackground(ContextCompat.getDrawable(this, R.drawable.material_bg));
+        }
+    }
+}
---
+++ app/src/main/java/com/gabilheri/moviestmdb/ui/detail/MovieDetailsViewHolder.java
@@ -0,0 +1,8 @@
+package com.gabilheri.moviestmdb.ui.detail;
+
+/**
+ * Created by CPU11112-local on 9/15/2017.
+ */
+
+public class MovieDetailsViewHolder {
+}
