be9b24b9dd070592ac28623aff42365053205de5
Phong Huỳnh <p7157568@gmail.com>
2017-09-15 15:07:32 +0700

    feat - create movie detail presenter

--- app/src/main/java/com/gabilheri/moviestmdb/ui/detail/MovieDetailsDescriptionPresenter.java
+++ app/src/main/java/com/gabilheri/moviestmdb/ui/detail/MovieDetailsDescriptionPresenter.java
@@ -1,8 +1,33 @@
 package com.gabilheri.moviestmdb.ui.detail;
 
+import android.support.v17.leanback.widget.Presenter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gabilheri.moviestmdb.R;
+import com.gabilheri.moviestmdb.data.models.MovieDetails;
+
 /**
  * Created by CPU11112-local on 9/15/2017.
  */
 
-public class MovieDetailsDescriptionPresenter {
+public class MovieDetailsDescriptionPresenter extends Presenter {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.vh_details, parent, false);
+        return new MovieDetailsViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, Object item) {
+        MovieDetails movieDetails = (MovieDetails) item;
+        MovieDetailsViewHolder holder = (MovieDetailsViewHolder) viewHolder;
+        holder.bind(movieDetails);
+    }
+
+    @Override
+    public void onUnbindViewHolder(ViewHolder viewHolder) {
+
+    }
 }
