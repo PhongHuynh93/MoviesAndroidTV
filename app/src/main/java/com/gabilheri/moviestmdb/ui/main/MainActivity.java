86f13e0c76c387007af11254acc0752edc86b8a4
Phong Huỳnh <p7157568@gmail.com>
2017-09-18 09:40:58 +0700

    feat - add error fragment

--- app/src/main/java/com/gabilheri/moviestmdb/ui/main/MainActivity.java
+++ app/src/main/java/com/gabilheri/moviestmdb/ui/main/MainActivity.java
@@ -1,6 +1,10 @@
 package com.gabilheri.moviestmdb.ui.main;
 
 import android.os.Bundle;
+import android.support.v17.leanback.app.ErrorFragment;
+import android.view.View;
+
+import com.gabilheri.moviestmdb.R;
 import com.gabilheri.moviestmdb.ui.base.BaseTvActivity;
 
 
@@ -17,6 +21,21 @@ public class MainActivity extends BaseTvActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addFragment(MainFragment.newInstance());
+//        addFragment(MainFragment.newInstance());
+        addFragment(buildErrorFragment());
+    }
+
+    private ErrorFragment buildErrorFragment() {
+        ErrorFragment errorFragment = new ErrorFragment();
+        errorFragment.setTitle(getString(R.string.text_error_oops_title));
+        errorFragment.setMessage(getString(R.string.error_message_network_needed_app));
+        errorFragment.setButtonText(getString(R.string.text_close));
+        errorFragment.setButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        return errorFragment;
     }
 }
