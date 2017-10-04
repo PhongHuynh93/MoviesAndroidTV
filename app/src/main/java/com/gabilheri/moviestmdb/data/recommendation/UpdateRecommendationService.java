2f3aaff5a83f4c244e2b5b65343723a19d0f9ada
Phong Huỳnh <p7157568@gmail.com>
2017-10-04 14:11:10 +0700

    feat - generate pallete color for the recommendations in launcher

--- app/src/main/java/com/gabilheri/moviestmdb/data/recommendation/UpdateRecommendationService.java
+++ app/src/main/java/com/gabilheri/moviestmdb/data/recommendation/UpdateRecommendationService.java
@@ -11,16 +11,18 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 import android.support.app.recommendation.ContentRecommendation;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.graphics.Palette;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.Movie;
+import com.example.myapplication.data.models.PaletteColors;
 import com.example.myapplication.interactor.GetMovieList;
 import com.example.myapplication.module.HttpClientModule;
 import com.example.myapplication.util.Constant;
 import com.gabilheri.moviestmdb.App;
 import com.gabilheri.moviestmdb.R;
 import com.gabilheri.moviestmdb.ui.playback.PlaybackOverlayActivity;
+import com.gabilheri.moviestmdb.util.PaletteUtils;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -99,17 +101,21 @@ public class UpdateRecommendationService extends Service {
 
             for (int i = 0; i < movieList.size() && i < MAX_RECOMMENDATIONS; i++) {
                 Movie post = movieList.get(i);
+                // TODO: 10/4/2017 create palette color here
                 builder.setIdTag("Post" + i + 1)
+                        // step - the text in line 1
                         .setTitle(post.getTitle())
+                        // step - the text in line 2
+                        .setText(getString(R.string.browser_header_3))
                         .setProgress(100, 0)
                         .setSortKey("1.0")
                         .setAutoDismiss(true)
-                        .setColor(ContextCompat.getColor(getApplicationContext(), R.color.primary))
+                        // step - the color contains 2 texts, use the pallet color
+//                        .setColor(ContextCompat.getColor(getApplicationContext(), R.color.primary))
                         .setBackgroundImageUri(HttpClientModule.BACKDROP_URL + post.getBackdropPath())
                         .setGroup("Trending")
                         .setStatus(ContentRecommendation.CONTENT_STATUS_READY)
                         .setContentTypes(new String[]{ContentRecommendation.CONTENT_TYPE_VIDEO})
-                        .setText(getString(R.string.browser_header_3))
                         .setContentIntentData(ContentRecommendation.INTENT_TYPE_ACTIVITY,
                                 buildPendingIntent(movieList, post), 0, null);
 
@@ -120,6 +126,9 @@ public class UpdateRecommendationService extends Service {
                             .asBitmap()
                             .into(cardWidth, cardHeight) // Only use for synchronous .get()
                             .get();
+                    Palette palette = Palette.from(bitmap).generate();
+                    PaletteColors colors = PaletteUtils.getPaletteColors(palette);
+                    builder.setColor(colors.getToolbarBackgroundColor());
                     builder.setContentImage(bitmap);
 
                     // Create an object holding all the information used to recommend the content.
